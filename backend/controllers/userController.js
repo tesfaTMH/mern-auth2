753b0446866ed2b6f1d857fa9e5289b066881187
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-07-28 20:05:19 +0300

    Test api for user route added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,5 @@
+const userController = (req, res) => {
+  res.json({ message: "API testing route" });
+};
+
+export default userController;
--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/connectDB.js";
 
+import userRoute from "./routes/userRoute.js";
+
 dotenv.config();
 
 connectDB();
 const app = express();
 
 const PORT = 3000;
+
+app.use("/api/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
