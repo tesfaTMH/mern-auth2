38ccc01ccbcbb675671a9e92420b99563f4dd7b8
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-07-28 18:22:46 +0300

    Header backbone created

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="bg-slate-200">
+      <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+        <Link to="/">
+          <h1 className="font-bold">MERN Auth App</h1>
+        </Link>
+
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/sign-in">
+            <li>Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
