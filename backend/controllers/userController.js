0e2f7b0a83f0fa2f263608d3515732a12dce2439
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-07-28 20:58:28 +0300

    Backend logic for signing up a user added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,3 @@
-const userController = (req, res) => {
+export const userController = (req, res) => {
   res.json({ message: "API testing route" });
 };
-
-export default userController;
--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,18 @@ import dotenv from "dotenv";
 import connectDB from "./config/connectDB.js";
 
 import userRoute from "./routes/userRoute.js";
+import authRoute from "./routes/authRoute.js";
 
 dotenv.config();
 
 connectDB();
 const app = express();
+app.use(express.json());
 
 const PORT = 3000;
 
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
