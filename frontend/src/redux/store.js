4ce694fe8fc94d29051b7c7c47a13e6295bc9cda
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-07-30 18:14:42 +0300

    Frontend persist functionality data added to keep user information after page refresh

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+// combine reducers incase you have more than one using
+const rootReducer = combineReducers({ user: userReducer });
+
+// create a persisted version to keep the data after
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
