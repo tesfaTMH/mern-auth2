4a4f3e45f5cea1ea5beca16de7dd2af18cb772c4
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-07-30 13:03:18 +0300

    Backend login functionality with JWT cookie added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
 import { customErrorHandler } from "../utils/customErrorHandler.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,5 +13,28 @@ export const signup = async (req, res, next) => {
   } catch (err) {
     next(err);
     //next(customErrorHandler(303, "Something went wrong"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound) {
+      return next(customErrorHandler(404, "User not found"));
+    }
+    const matchedPassword = bcryptjs.compareSync(password, userFound.password);
+    if (!matchedPassword) {
+      return next(customErrorHandler(401, "Invalid credentials"));
+    }
+    const token = jwt.sign({ id: userFound._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...userDetails } = userFound._doc;
+    const expireDate = new Date(Date.now() + 3600000);
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expireDate })
+      .status(200)
+      .json(userDetails);
+  } catch (err) {
+    next(err);
   }
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
   },
-  plugins: [],
-};
+});
