8f311b9c0edbbf302511a144db6f11cbdc7449ce
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-07-31 06:22:51 +0300

    Frontend profile updated to include logics for updating  profile image

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,23 +1,91 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [imageFile, setImageFile] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [userFormData, setUserFormData] = useState({});
+  console.log(userFormData);
+  useEffect(() => {
+    if (imageFile) {
+      handleFileUpload(imageFile);
+    }
+  }, [imageFile]);
+
+  const handleFileUpload = async (imageFile) => {
+    const storage = getStorage(app);
+    const imageFileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, imageFileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+      },
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setUserFormData({ ...userFormData, profilePicture: downloadURL });
+        });
+      }
+    );
+  };
+
+  const handleChange = (e) => {
+    setUserFormData({ ...userFormData, [e.target.id]: e.target.value });
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-5">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setImageFile(e.target.files[0])}
+        />
         <img
           className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2"
-          src={currentUser.profilePicture}
+          src={currentUser.profilePicture || userFormData.profilePicture}
           alt="profile"
+          onClick={() => fileRef.current.click()}
         />
+        <p className="text-sm self-center">
+          {imageError ? (
+            <span className="text-red-700">
+              Error uploading image (file size must be less than 2MB)
+            </span>
+          ) : imagePercent > 0 && imagePercent < 100 ? (
+            <span className="text-slate-700">{`Uploading: ${imagePercent}`}</span>
+          ) : imagePercent === 100 ? (
+            <span className="text-green-700">Image uploaded successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="text"
           id="username"
           placeholder="Username"
           className="bg-slate-100 rounded-lg p-3"
           defaultValue={currentUser.username}
+          onChange={handleChange}
         />
         <input
           type="email"
@@ -25,12 +93,14 @@ const Profile = () => {
           placeholder="Email"
           className="bg-slate-100 rounded-lg p-3"
           defaultValue={currentUser.email}
+          onChange={handleChange}
         />
         <input
           type="password"
           id="password"
           placeholder="Password"
           className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           update
