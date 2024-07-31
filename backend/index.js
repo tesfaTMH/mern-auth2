a39f8a520106fbaf74ffbb9e27bb096ebd593441
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-07-31 17:02:46 +0300

    add pre production changes

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,18 @@ import authRoute from "./routes/authRoute.js";
 
 import { errorHandler } from "./middleware/errorHandler.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 connectDB();
 const app = express();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 app.use(express.json());
 app.use(cookieParser());
 
